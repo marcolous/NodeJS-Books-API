f51720df5a9ef9896f0dcdc592e9ea75e6c50ef6
marcolous <louismarco226@gmail.com>
2025-07-01 22:14:42 +0300

    create book route

---
+++ routes/books.js
@@ -0,0 +1,80 @@
+import express from "express";
+import Joi from "joi";
+
+const router = express.Router();
+
+const books = [
+  {
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    description: "A journey of a shepherd boy to find his destiny.",
+    price: 10.99,
+    cover: "https://example.com/alchemist.jpg",
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    description:
+      "An easy & proven way to build good habits and break bad ones.",
+    price: 14.99,
+    cover: "https://example.com/atomic-habits.jpg",
+  },
+];
+
+const schema = Joi.object({
+  title: Joi.string().trim().min(3).required(),
+  author: Joi.string().trim().min(3).required(),
+  description: Joi.string().trim().min(3).required(),
+  price: Joi.number().min(0).required(),
+  cover: Joi.string().trim().min(3).required(),
+});
+
+router.get("/", (req, res) => {
+  res.status(200).json(books);
+});
+
+router.get("/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404).json({
+      message: "Book not found",
+    });
+  }
+});
+router.post("/", (req, res) => {
+  const isValid = schemaValidation(req.body, res);
+  if (!isValid) return;
+
+  const book = {
+    id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    price: req.body.price,
+    cover: req.body.cover,
+  };
+
+  books.push(book);
+  res.status(201).json(book);
+});
+
+function schemaValidation(obj, res) {
+  const { error } = schema.validate(obj, { abortEarly: false });
+  if (error) {
+    res.status(400).json({
+      status: 400,
+      errors: error.details.map((detail) => ({
+        field: detail.path.join("."),
+        message: detail.message,
+      })),
+    });
+    return false;
+  }
+  return true;
+}
+
+export default router;
--- server.js
+++ server.js
@@ -1,76 +1,12 @@
 import express from "express";
-import Joi from "joi";
+import bookPath from "./routes/books.js";
 
 const app = express();
 app.use(express.json());
 const PORT = 5000;
-const books = [
-  {
-    id: 1,
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    description: "A journey of a shepherd boy to find his destiny.",
-    price: 10.99,
-    cover: "https://example.com/alchemist.jpg",
-  },
-  {
-    id: 2,
-    title: "Atomic Habits",
-    author: "James Clear",
-    description:
-      "An easy & proven way to build good habits and break bad ones.",
-    price: 14.99,
-    cover: "https://example.com/atomic-habits.jpg",
-  },
-];
 
-const schema = Joi.object({
-  title: Joi.string().trim().min(3).required(),
-  author: Joi.string().trim().min(3).required(),
-  description: Joi.string().trim().min(3).required(),
-  price: Joi.number().min(0).required(),
-  cover: Joi.string().trim().min(3).required(),
-});
+app.use("/api/books", bookPath);
 
-app.get("/api/books", (req, res) => {
-  res.status(200).json(books);
-});
-
-app.get("/api/books/:id", (req, res) => {
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (book) {
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({
-      message: "Book not found",
-    });
-  }
-});
-
-app.post("/api/books", (req, res) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
-  if (error) {
-    return res.status(400).json({
-      status: 400,
-      errors: error.details.map((detail) => ({
-        field: detail.path.join("."),
-        message: detail.message,
-      })),
-    });
-  }
-  const book = {
-    id: books.length + 1,
-    title: req.body.title,
-    author: req.body.author,
-    description: req.body.description,
-    price: req.body.price,
-    cover: req.body.cover,
-  };
-
-  res.status(201).json(book);
-  books.push(book);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
